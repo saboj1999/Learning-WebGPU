5098d8082c27fe6a6fa2dc4f12a6e902abd88325
John Sabo <saboj@moravian.edu>
2023-09-07 22:59:18 -0400

    Hello Triangle WebGPU

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learning-WebGPU
+1) Install Node and npm
+2) Run: "npm install"
+3) Run: "npm start"
+4) Navigate to "localhost:8080"
---
+++ src/main.ts
@@ -0,0 +1,69 @@
+// const output_label : HTMLElement = <HTMLElement> document.getElementById("compatibility-label");
+
+
+// if (navigator.gpu) {
+//     output_label.innerText = "WebGPU is supported on this browser";
+// }
+// else {
+//     output_label.innerText = "WebGPU is not supported on this browser";
+// }
+
+import shader from "./shaders.wgsl"
+
+const Initialize = async() => 
+{
+    const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("gfx-main");
+    const adapter : GPUAdapter = <GPUAdapter> await navigator.gpu?.requestAdapter();
+    const device : GPUDevice = <GPUDevice> await adapter?.requestDevice();
+    const context : GPUCanvasContext = <GPUCanvasContext> canvas.getContext("webgpu");
+    const format : GPUTextureFormat = "bgra8unorm";
+    context.configure({
+        device: device,
+        format: format,
+    });
+
+    const pipeline : GPURenderPipeline = device.createRenderPipeline({
+        vertex : {
+            module: device.createShaderModule({
+                code: shader
+            }),
+            entryPoint: "vs_main",
+        },
+
+        fragment : {
+            module: device.createShaderModule({
+                code: shader
+            }),
+            entryPoint: "fs_main",
+            targets: [{
+                format: format
+            }]
+        },
+
+        primitive : {
+            topology: "triangle-list"
+        },
+
+        layout: "auto"
+    });
+
+    const commandEncoder : GPUCommandEncoder = device.createCommandEncoder();
+    const textureView : GPUTextureView = context.getCurrentTexture().createView();
+    const renderpass : GPURenderPassEncoder = commandEncoder.beginRenderPass({
+        colorAttachments: [{
+            view: textureView,
+            clearValue: {r: 0.5, g: 0.0, b: 0.25, a: 1.0},
+            loadOp: "clear",
+            storeOp: "store"
+        }]
+    });
+    
+    renderpass.setPipeline(pipeline);
+    renderpass.draw(3, 1, 0, 0);
+    renderpass.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+}
+
+Initialize();
+
